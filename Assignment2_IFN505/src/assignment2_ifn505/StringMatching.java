f3a277167df595ddb99fef600d5b3b09c4dbd52e
Hiroki <mitsuke1116@gmail.com>
2015-10-12 14:58:05 +1000

    Start

---
+++ Assignment2_IFN505/src/assignment2_ifn505/StringMatching.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_ifn505;
+
+/**
+ *
+ * @author takaha
+ */
+public final class StringMatching {
+
+    private StringMatching() {
+    }
+
+    public int BruteForceStringMathch(char[] text, char[] pattern){
+        int index = 0;
+        
+        return index;
+    }
+    
+    //Table consists of two arrays, one is of char and the other is of int
+   // public int[] 
+}
